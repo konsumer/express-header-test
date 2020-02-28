a9ce18f3cf16a6f10373e2a1ba70ec0538c33610
David Konsumer <konsumer@jetboystudio.com>
2020-02-28 13:50:45 -0800

    added 1-by-1 output

--- public/index.js
+++ public/index.js
@@ -1,14 +1,31 @@
 /* global fetch */
 
 window.onload = async () => {
-  const [preOutput, preHeaders] = document.querySelectorAll('pre')
+  const [preOutput, preHeaders, preOutSingleHeaders] = document.querySelectorAll('pre')
+
+  const testHeaders = JSON.parse(preHeaders.innerText)
 
   // fill testing headers
   const headers = {}
-  JSON.parse(preHeaders.innerText).forEach(k => {
+  testHeaders.forEach(k => {
     headers[k] = Math.random()
   })
 
   // get full headers
   preOutput.innerHTML = JSON.stringify(await fetch('/header', { headers }).then(r => r.json()), null, 2)
+
+  const singleOutput = {}
+
+  // get single headers from server
+  const out = await Promise.all(testHeaders.map(k => {
+    return fetch(`/header/${k}`, { headers }).then(r => r.json())
+  }))
+
+  testHeaders.forEach((k, i) => {
+    singleOutput[k] = out[i]
+  })
+
+  preOutSingleHeaders.innerHTML = JSON.stringify(singleOutput, null, 2)
+
+  console.log(out)
 }

245e557bc45a8b67d56cab78b45f3fb8832019e5
David Konsumer <konsumer@jetboystudio.com>
2020-02-28 13:42:33 -0800

    nicer naming for vars

--- public/index.js
+++ public/index.js
@@ -1,14 +1,14 @@
 /* global fetch */
 
 window.onload = async () => {
-  const outputs = [...document.querySelectorAll('pre')]
+  const [preOutput, preHeaders] = document.querySelectorAll('pre')
 
   // fill testing headers
   const headers = {}
-  JSON.parse(outputs[1].innerText).forEach(k => {
+  JSON.parse(preHeaders.innerText).forEach(k => {
     headers[k] = Math.random()
   })
 
   // get full headers
-  outputs[0].innerHTML = JSON.stringify(await fetch('/header', { headers }).then(r => r.json()), null, 2)
+  preOutput.innerHTML = JSON.stringify(await fetch('/header', { headers }).then(r => r.json()), null, 2)
 }
